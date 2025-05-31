710127ea009a4ed3dd2573576b3079ee72ca533e
000M000 <hamidmohammed76@gmail.com>
2025-05-31 17:20:13 -0230

    Feature: App Routing with GoRouter

---
+++ lib/screens/chart_screens/pie_chart_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PieChartScreen extends StatelessWidget {
+  const PieChartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pie Chart'),
+      ),
+      body: const Center(
+        child: Text('Pie Chart Screen - Coming Soon!'),
+      ),
+    );
+  }
+}
